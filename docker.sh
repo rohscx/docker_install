328504e8e7f954b8acaa60e5d1653a06299cc8ec
rohscx <rohscx@gmail.com>
2017-03-25 16:50:15 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,21 +1,34 @@
 #! /bin/bash
 
+echo "Running apt-get"
+
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-    
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "apt-get complete"  
 
+echo "Running running curl for the Docker key and adding it with apt-key"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "Key grab complete"
+
+echo "Adding Docker respository"
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-   
+echo "repository add complete"
+
+echo "Running apt-get update"
+sudo apt-get udpate
+echo "update complete"
+
+echo "Installing Docker from apt-get"
 sudo apt-get install docker-ce
+echo "install complete"
 
+echo "Starting Docker service"
 sudo service docker start
-	if service is not running
-
 sudo docker inspect --format '{{.State.Status}} {{.NetworkSettings.IPAddress}} {{.Config.Labels.name}} {{.Config.Image}}' $(sudo docker ps -aq)
+echo "Script complete"
